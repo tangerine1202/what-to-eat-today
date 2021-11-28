46ec0942c7140d17cb04ee5f482845b250f706f5
Alan Huang <alan0988469026@gmail.com>
2021-11-29 00:09:04 +0800

    add linter

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 
 import line from '@line/bot-sdk'
 import express from 'express'
@@ -9,15 +9,15 @@ dotenv.config()
 // create LINE SDK config from env variables
 const config = {
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-  channelSecret: process.env.CHANNEL_SECRET,
-};
+  channelSecret: process.env.CHANNEL_SECRET
+}
 
 // create LINE SDK client
-const client = new line.Client(config);
+const client = new line.Client(config)
 
 // create Express app
 // about Express itself: https://expressjs.com/
-const app = express();
+const app = express()
 
 // register a webhook handler with middleware
 // about the middleware, please refer to doc
@@ -26,27 +26,27 @@ app.post('/callback', line.middleware(config), (req, res) => {
     .all(req.body.events.map(handleEvent))
     .then((result) => res.json(result))
     .catch((err) => {
-      console.error(err);
-      res.status(500).end();
-    });
-});
+      console.error(err)
+      res.status(500).end()
+    })
+})
 
 // event handler
-function handleEvent(event) {
+function handleEvent (event) {
   if (event.type !== 'message' || event.message.type !== 'text') {
     // ignore non-text-message event
-    return Promise.resolve(null);
+    return Promise.resolve(null)
   }
 
   // create a echoing text message
-  const echo = { type: 'text', text: event.message.text };
+  const echo = { type: 'text', text: event.message.text }
 
   // use reply API
-  return client.replyMessage(event.replyToken, echo);
+  return client.replyMessage(event.replyToken, echo)
 }
 
 // listen on port
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 app.listen(port, () => {
-  console.log(`listening on ${port}`);
-});
+  console.log(`listening on ${port}`)
+})
