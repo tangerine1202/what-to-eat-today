2322251ed07e6e5928dc64df95c61d3b2b8bebdc
Alan Huang <alan0988469026@gmail.com>
2021-12-01 01:50:47 +0800

    Fix echo

--- service/echo.js
+++ service/echo.js
@@ -1,6 +1,5 @@
 import { replyText } from '../lib/replyHelper.js'
 
 export default function (replyToken, { text }) {
-  const echo = { type: 'text', text }
-  return replyText(replyToken, echo)
+  return replyText(replyToken, text)
 }
